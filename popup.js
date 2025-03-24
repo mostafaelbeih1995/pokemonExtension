e184d0481dc02ddf850b65ad5e3597f7ebc4e83e
Mostafa Elbeih <mostafa.elbeihjr@gmail.com>
2025-03-24 19:40:41 +0100

    small edit

--- popup.js
+++ popup.js
@@ -104,3 +104,21 @@ function showPokemon(response){
     
     // catchPokemon(response)
 }
+
+const pokeballs = document.querySelectorAll(".pokeball");
+    const backgrounds = [
+        './assets/forest.webp',
+        'background2.jpg',
+        'background3.jpg',
+        'background4.jpg'
+    ];
+
+    function switchBackground() {
+        const background1 = document.querySelector('.background-1');
+        const background2 = document.querySelector('.background-2');
+        const randomIndex = Math.floor(Math.random() * backgrounds.length);
+        background1.style.backgroundImage = `url('./assets/battleback${randomIndex+1}-1.png')`;
+        background2.style.backgroundImage = `url('./assets/battleback${randomIndex+1}-2.png')`;
+    }
+
+    switchBackground();
